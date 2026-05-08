05e2eb05561bd04b770132852120ed1f72e99fea
Mursalin Ahmad Khan <7mursalinahmadkhan@gmail.com>
2026-05-08 16:59:44 +0500

    Update BMI_app.py

--- BMI_app.py
+++ BMI_app.py
@@ -16,36 +16,29 @@ gender = st.radio("🧬 Select your gender:", ("Male", "Female"))
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    feet = st.number_input(
-        "Height (feet):",
+    feet = st.slider(
+        "Height (feet)",
         min_value=1,
         max_value=8,
-        value=5,
-        step=1
+        value=5
     )
 
 with col2:
-    inches = st.number_input(
-        "Height (inches):",
+    inches = st.slider(
+        "Height (inches)",
         min_value=0,
         max_value=11,
-        value=7,
-        step=1
+        value=7
     )
 
 with col3:
-    weight = st.number_input(
-        "Weight (kg):",
+    weight = st.slider(
+        "Weight (kg)",
         min_value=1.0,
         max_value=120.0,
         value=70.0,
-        step=0.1,
-        format="%.3f"
+        step=0.1
     )
-
-# Extra validation
-if weight > 120:
-    st.error("Weight cannot exceed 120 kg")
 
 # Calculate BMI Button 
 if st.button("Calculate BMI"):
